c76ae5073d305b3efe5d47eca4792aaf394deb91
Galen Miller-Atkins <33107415+GMillerA@users.noreply.github.com>
2018-04-16 09:52:25 -0700

    Add files via upload
    v.1 of text-based dnd game. Need to add in character classes and a replay option

---
+++ days/13-15-text-games/actors.py
@@ -0,0 +1,59 @@
+import random
+
+
+class Creature:
+    def __init__(self, name, level, health):
+        self.name = name
+        self.level = level
+        self.health = health
+
+
+
+    def defensive_roll(self):
+        roll = random.randint(1, 12)
+        return roll * self.level
+
+
+class Dragon(Creature):
+    def __init__(self, name, level, health, scaliness, breaths_fire):
+        super().__init__(name, level, health)
+        self.health = health
+        self.scaliness = scaliness
+        self.breaths_fire = breaths_fire
+
+    def defensive_roll(self):
+        roll = super().defensive_roll()
+        value = roll * self.scaliness
+        if self.breaths_fire:
+            value = value * 2
+        return value
+
+
+class Wizard(Creature): #TODO: Change wizard to general hero? Add sublasses for Ranger, Fighter, etc.?
+    def __init__(self, name, level, health, experience=0):
+        super().__init__(name, level, health)
+        self.experience = experience
+
+
+    def attack(self, creature):
+        my_roll = self.defensive_roll()
+        their_roll = creature.defensive_roll()
+        if my_roll >= their_roll:
+            creature.health -= self.level
+            if creature.health <= 0:
+                return True
+        else:
+            self.health -= creature.level
+            print('Ouch! Current health is now {}'.format(self.health))
+            return False
+
+    def update_experience(self, creature):
+        exp = creature.level * 1.5
+        self.experience += exp
+
+    def level_up(self):
+        if self.experience >= (.5 * self.level):
+            self.level += 5 #TODO: scale levelling up by creature level?
+            self.experience = 0
+            self.health = self.level
+            print('The hero {} is now level {}!'.format(self.name, self.level))
---
+++ days/13-15-text-games/game.py
@@ -0,0 +1,75 @@
+from actors import Creature, Wizard, Dragon
+import random
+
+def main():
+    print_header()
+    #create_hero()
+    game_loop()
+
+def create_hero():
+    name = input("Enter Hero's Name: ")
+    level = random.randint(50,90)
+    hero = Wizard(name, level, health=level)
+    print('A level {} Wizard named {} appears!'.format(hero.level, hero.name))
+    return hero
+
+def print_header():
+    print('---------------------------------')
+    print('          OFF-BRAND DND GAME')
+    print('---------------------------------')
+    print()
+
+
+def game_loop():
+    creatures = [
+        Creature('Bat', 5, 5),
+        Creature('Toad', 1, 1),
+        Creature('Tiger', 12, 12),
+        Dragon('Black Dragon', 50, 50, scaliness=2, breaths_fire=False),
+        Wizard('Evil wizard', 150, 150),
+    ]
+
+    hero = create_hero()
+
+    while True:
+
+        active_creature = random.choice(creatures)
+
+        print('A {} of level {} appears from a dark and foggy forest...'
+              .format(active_creature.name, active_creature.level))
+        print()
+
+        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
+        if cmd == 'a':
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+                hero.level_up()
+                hero.update_experience(active_creature)
+                print("The wizard {} defeated {}".format(hero.name, active_creature.name))
+            else:
+                print("The wizard {} has been hurt by the powerful {}".format(hero.name, active_creature.name))
+                if hero.health <= 0:
+                    print("The hero {} has perished!".format(hero.name))
+                    break
+        elif cmd == 'r':
+            print('The wizard {} has become unsure of his power and flees!!!'.format(hero.name))
+        elif cmd == 'l':
+            print('The wizard {} takes in the surroundings and sees:'
+                  .format(hero.name))
+            for c in creatures:
+                print(" * {} of level {}".format(
+                    c.name, c.level
+                ))
+        else:
+            print("OK, exiting game... bye!")
+            break
+
+        if not creatures:
+            print("You've defeated all the creatures, well done!")
+            break
+
+        print()
+
+
+if __name__ == '__main__':
+    main()
